1304b65bb3f0cae0df441ce16794f71dae6668e8
checkandmate9 <bilalsmv09@gmail.com>
2024-03-12 02:58:06 +0300

    olsana

---
+++ script.js
@@ -0,0 +1,34 @@
+const answer = Math.floor(Math.random() * 10 + 1)
+let guesses = 0;
+
+
+
+document.getElementById("submitButton").onclick = function(){ 
+    gönder()
+}
+function gönder(){
+    
+    let guess = document.getElementById("guessField").value
+    guesses+=1;
+
+    
+    if(guess == answer){
+    alert(`${answer} is the #. It took you ${guesses} guesses`)
+    guesses = 0;
+    
+     }
+    else if (guess < answer){
+    alert("Too small!")
+     }
+    else {
+    alert("Too large!")
+    }
+    
+
+}
+document.getElementById("guessField").addEventListener("keyup", function(e){
+    if(e.key === "Enter"){
+        gönder();
+    }
+    
+});
